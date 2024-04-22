526464b5f68c20c08cbde3633106c407db22849e
Azara <yyushahu02@gmail.com>
2024-04-22 23:57:11 +0000

    first api call to rest api test web that returns todo progress of employees

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''A python script that returns info about an employees' TODO list
+progress
+'''
+import requests
+
+
+def get_todo_progress(employee_id):
+    '''fetches employee todo progress
+
+    Args:
+        employee_id (int): Employee ID
+    '''
+    # fetch employee info
+    api_url = f'https://jsonplaceholder.typicode.com'
+    employee_info = requests.get(f'{api_url}/users/{employee_id}')
+    # raise stored HTTPError, if one occured
+    employee_info.raise_for_status()
+    employee_data = employee_info.json()
+    employee_name = employee_data.get("name")
+    # rectified error: "" instead of ''
+
+    # fetch todos for employee
+    todos = requests.get(f'{api_url}/users/{employee_id}/todos')
+    todos.raise_for_status()
+    todos_data = todos.json()
+
+    # count completed and total tasks
+    total_tasks = len(todos_data)
+    completed_tasks = 0
+    for todo in todos_data:
+        if todo.get("completed"):  # rectified error: "" instead of ''
+            completed_tasks += 1
+
+    # display progress
+    print(
+        f'Employee {employee_name} is done with tasks '
+        f'({completed_tasks}/{total_tasks}):'
+    )
+    for todo in todos_data:
+        if todo.get("completed"):  # rectified error: "" instead of ''
+            print(f"\t {todo.get('title')}")
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <employee_id>')
+        sys.exit(1)
+
+    try:
+        employee_id = sys.argv[1]
+    except ValueError:
+        print('Employee ID must be an integer.')
+        sys.exit(1)
+
+    # run function
+    get_todo_progress(employee_id)
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# API
+This directory contains beginner python scripts on API.
