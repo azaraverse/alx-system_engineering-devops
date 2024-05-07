b4f856aa4fae0d4888f1c7484957d3b220908224
Azara <yyushahu02@gmail.com>
2024-05-07 14:27:09 +0000

    add custom header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,13 +14,13 @@ def number_of_subscribers(subreddit):
       (int) 0 if not a valid subreddit
     '''
     url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-custom-header/0.0.1'}
 
-    try:
-        response = requests.get(f'{url}{subreddit}/about', allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        dictionary = response.json()
-        data = dictionary.get("data")
-        return data.get("subscribers")
-    except (requests.RequestException, ValueError, KeyError):
+    response = requests.get(
+        f'{url}{subreddit}/about', headers=headers, allow_redirects=False
+        )
+    if response.status_code != 200:
         return 0
+    dictionary = response.json()
+    data = dictionary.get("data")
+    return data.get("subscribers")
